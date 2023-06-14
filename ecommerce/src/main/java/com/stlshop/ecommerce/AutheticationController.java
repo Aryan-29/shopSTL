5bb796f15aef6da3638bc8b0bdd529871d59b812
Aryan Parekh <aryanparek2908@gmail.com>
2023-06-15 01:39:42 +0530

    Validation added to Password field

--- ecommerce/src/main/java/com/stlshop/ecommerce/AutheticationController.java
+++ ecommerce/src/main/java/com/stlshop/ecommerce/AutheticationController.java
@@ -1,12 +1,18 @@
 package com.stlshop.ecommerce;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -18,7 +24,18 @@ public class AutheticationController {
 
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
-            @RequestBody RegisterRequest request) {
+            @RequestBody @Valid RegisterRequest request, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            List<String> errorMessages = new ArrayList<>();
+            List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+            for (FieldError fieldError : fieldErrors) {
+                errorMessages.add(fieldError.getDefaultMessage());
+            }
+
+            AuthenticationResponse response = new AuthenticationResponse();
+            response.setErrorMessage(String.join(", ", errorMessages));
+            return ResponseEntity.badRequest().body(response);
+        }
         return ResponseEntity.ok(service.register(request));
     }
 
--- ecommerce/src/main/java/com/stlshop/ecommerce/RegisterRequest.java
+++ ecommerce/src/main/java/com/stlshop/ecommerce/RegisterRequest.java
@@ -1,5 +1,7 @@
 package com.stlshop.ecommerce;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,5 +13,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RegisterRequest {
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password should have minimum 8 characters")
     private String password;
 }
